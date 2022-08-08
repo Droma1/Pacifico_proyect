75d9542fb1f7edd2bafab93e5b3bdda1b8bb4cfb
Droma1 <nando.rex333@gmail.com>
2022-08-07 22:19:57 -0500

    cambio new 07/08/2022

--- Config/configGeneral.php
+++ Config/configGeneral.php
@@ -1,5 +1,5 @@
 <?php
-	const SERVERURL="http://localhost/Pacifico/";
+	const SERVERURL="http://localhost/web/Pacifico/";
 	const COMPANY="Pacifico Supermercados";
 	date_default_timezone_set("America/Bogota");
 ?>
